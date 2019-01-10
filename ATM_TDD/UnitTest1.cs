d18ba862a45856565d18fd423d51d7e8576d8a6c
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-09 17:54:23 -0800

    all tests pssing

--- ATM_TDD/UnitTest1.cs
+++ ATM_TDD/UnitTest1.cs
@@ -13,4 +13,32 @@ namespace ATM_TDD
             Assert.Equal(5050.ToString(), balance);
         }
     }
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            string balance = Program.AddToBalance(4000, 50).ToString();
+            Assert.Equal(4050.ToString(), balance);
+        }
+    }
+
+    public class UnitTest3
+    {
+        [Fact]
+        public void Test3()
+        {
+            string balance = Program.SubtractFromBalance(4000, 100).ToString();
+            Assert.Equal(3900.ToString(), balance);
+        }
+    }
+    public class UnitTest4
+    {
+        [Fact]
+        public void Test4()
+        {
+            string balance = Program.SubtractFromBalance(4000, 1000).ToString();
+            Assert.Equal(3000.ToString(), balance);
+        }
+    }
 }
